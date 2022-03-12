571b79ae80f2dfaacb735c8d902d73c64a060772
diptasahacse <diptasaha.lpu.cse@gmail.com>
2022-03-13 02:16:13 +0600

    == vs ===

---
+++ js/double-equal-and-triple-equal.js
@@ -0,0 +1,18 @@
+// Double Equal ( == ):
+/*
+Double equal (==) is only check weather the operands or value both are equal or not. It does not check the operands data type.
+*/
+console.log(5 == "5") // true
+// in above code both value are looks same 1st one is 5 as well as second one is 5 also, but both value type are different. first value is Number type and second value is String type. Double equal operator does not check the value's type.
+
+
+
+// Triple Equal ( === ):
+/*
+Triple equal (===) is check two things.
+1. Weather the operands or value both are equal or not.
+2. It does check the operands data type.
+*/
+console.log(5 === "5") // false
+console.log(5 == 5) // true
+// in above first code, both value are looks same 1st one is 5 as well as second one is 5 also, but both value type are different. first value is Number type and second value is String type. that's why it returns false. and in second code, both value are looks same 1st one is 5 as well as second one is 5 also, and also both type are same. first value is Number type and second value is also Number type. that's why it returns true. 
