627a79cc38f8a7204d109dcfee8132a0de5e8511
diptasahacse <diptasaha.lpu.cse@gmail.com>
2022-03-13 16:07:14 +0600

    array.map()

---
+++ js/array.js
@@ -0,0 +1,18 @@
+let arr = [2, 4, 6, 8, 10, 12, 14, 16];
+// With Simple Function Call
+// let newArr = arr.map(myFun);
+
+// function myFun(element, index, arr) {
+//     return element + 5;
+
+// }
+
+// With Anonymous function
+// let newArr = arr.map(function(element, index, arr) {
+//     return element + 5
+
+// });
+
+// With Arrow Function
+// let newArr = arr.map((element, index, arr) => element + 5);
+// console.log(newArr)
